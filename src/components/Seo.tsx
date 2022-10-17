c8c1129bbfa0218c5c0a5e9353c8ec2664ac0db5
Vasili Zalimidis <hello@zalimidis.dev>
2022-10-17 23:00:37 +0200

    fix: nextjs version

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -54,21 +54,6 @@ export default function Seo(props: SeoProps) {
       <meta name='twitter:title' content={meta.title} />
       <meta name='twitter:description' content={meta.description} />
       <meta name='twitter:image' content={meta.image} />
-      {meta.date && (
-        <>
-          <meta property='article:published_time' content={meta.date} />
-          <meta
-            name='publish_date'
-            property='og:publish_date'
-            content={meta.date}
-          />
-          <meta
-            name='author'
-            property='article:author'
-            content='Theodorus Clarence'
-          />
-        </>
-      )}
 
       {/* Favicons */}
       {favicons.map((linkProps) => (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
 import ButtonLink from '@/components/links/ButtonLink';
-import CustomLink from '@/components/links/CustomLink';
 import Seo from '@/components/Seo';
 
 export default function HomePage() {
@@ -13,28 +12,15 @@ export default function HomePage() {
         <section className='bg-gray-100'>
           <div className='flex flex-col items-center justify-center min-h-screen text-center layout'>
             <h1>Open Graph Generator</h1>
-            <p className='mt-2 text-sm text-gray-600'>
-              Made for personal use using{' '}
-              <CustomLink href='https://github.com/neg4n/next-api-og-image'>
-                next-api-og-image
-              </CustomLink>
-            </p>
-            <div className='flex flex-wrap gap-2 mt-4 text-sm text-gray-500'>
-              <ButtonLink href='https://github.com/theodorusclarence/og'>
-                See the repository
-              </ButtonLink>
-              <ButtonLink variant='light' href='/builder'>
-                Link Builder
-              </ButtonLink>
-            </div>
 
-            <footer className='absolute text-gray-800 bottom-2'>
-              © {new Date().getFullYear()} By{' '}
-              <CustomLink href='https://theodorusclarence.com?ref=og'>
-                Theodorus Clarence
-              </CustomLink>
-            </footer>
+            <ButtonLink variant='light' href='/builder'>
+              Link Builder
+            </ButtonLink>
           </div>
+
+          <footer className='absolute text-gray-800 bottom-2'>
+            © {new Date().getFullYear()} By{' '}
+          </footer>
         </section>
       </main>
     </>
